c5764aec786c94a44ce93ac24ab449a7d2796291
GogulskiL <lukaszgogul@gmail.com>
2022-09-26 22:55:58 +0200

    small check

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
+from genericpath import exists
 import os
+from os import path
 import time
 
-path_test = "testy_photo_checker\FB_IMG_1652721744105.jpg"
+path_test = "..."
 
-# loading the file creation date
+
 def get_time_created(path):
+    # loading the photo creation date
     c_time = os.path.getctime(path)
     local_time = time.ctime(c_time)
     return local_time
 
-# new photo name, change the colon to a hyphen
+
 def new_file_name(path):
+    # new photo name, change the colon to a hyphen
     get_new_name = str(get_time_created(path)).split(" ")
     new_name = "_".join(get_new_name).replace(":", "-")
     return new_name
 
 
-
-
-def resault(path):
+def result_new_photo(path):
+    # renaming and saving a new photo
     old_file = path
     new_photo = new_file_name(old_file) + ".jpg"
     return os.rename(old_file, new_photo)
-
-print(resault(path_test))
