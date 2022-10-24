cab10e3d07bd974d919538affc84a84a0486d530
GogulskiL <lukaszgogul@gmail.com>
2022-10-24 21:15:08 +0200

    try solving problems

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import time
 
 
 def get_folder_path():
-    folder_path = input("Podaj ścieżkę do folderu:  ")
+    folder_path = str(input('Podaj ścieżkę do folderu:  '))
     return folder_path
+
 
 def list_photo_in_folder(folder_path):
     return(os.listdir(folder_path))
+
 
 def get_time_created(path):
     # loading the photo creation date
@@ -18,7 +20,8 @@ def get_time_created(path):
 
 def new_file_name(path):
     # new photo name, change the colon to a hyphen
-    new_name = "_".join(str(get_time_created(path)).split(" ")).replace(":", "-")
+    new_name = "_".join(
+        str(get_time_created(path)).split(" ")).replace(":", "-")
     return new_name
 
 
@@ -26,7 +29,8 @@ def result_new_photo(path):
     # renaming and saving a new photo
     old_file = path
     new_photo = new_file_name(old_file) + ".jpg"
-    return os.rename(os.path.join(path,old_file),os.path.join(path,new_photo))
+    return os.rename(os.path.join(path, old_file), os.path.join(path, new_photo))
+
 
 def main():
     path = get_folder_path()
@@ -36,4 +40,3 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
