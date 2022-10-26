e71a4b2be83e7645aca8e3c65406ae047eb3a05c
GogulskiL <lukaszgogul@gmail.com>
2022-10-26 23:10:31 +0200

    solved the prev problem with loop, first working version

--- main.py
+++ main.py
@@ -11,34 +11,35 @@ def list_photo_in_folder(dir_path):
     return(os.listdir(dir_path))
 
 
-def get_time_created(i):
+def get_time_created(path_single_photo):
     # loading the photo creation date
-    c_time = os.path.getmtime(i)
+    c_time = os.path.getmtime(path_single_photo)
     local_time = time.ctime(c_time)
     return local_time
 
 
-def new_file_name(dir_path, i):
+def new_file_name(dir_path, path_single_photo):
     # new photo name, change the colon to a hyphen
     new_name = str(
-        "_".join((get_time_created(dir_path + "/" + i)).split(" ")).replace(":", "-"))
+        "_".join((get_time_created(dir_path + "/" + path_single_photo)).split(" ")).replace(":", "-"))
     return new_name
 
 
-def result_new_photo(dir_path, i):
+def result_new_photo(dir_path, path_single_photo):
     # renaming and saving a new photo
-    old_file = i
-    new_photo = new_file_name(dir_path, i) + ".jpg"
+    old_file = path_single_photo
+    new_photo = new_file_name(dir_path, path_single_photo) + ".jpg"
     return os.rename(os.path.join(dir_path, old_file), os.path.join(dir_path, new_photo))
 
 
-def main():
-    path = get_folder_path()
-    folder_content = list_photo_in_folder(path)
-    a = len(folder_content)
-    for i in range(0, a):
-        return result_new_photo(path, i)
+def check_rename_photo():
+    dir_path = get_folder_path()
+    folder_content = list_photo_in_folder(dir_path)
+    for path_single_photo in folder_content:
+        change_file = result_new_photo(dir_path, path_single_photo)
+    return change_file
 
 
 if __name__ == '__main__':
-    main()
+    
+    check_rename_photo()
