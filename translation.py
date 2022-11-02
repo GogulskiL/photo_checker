9964da1745ac8be5deb2e40bbf3427bc81f64710
GogulskiL <lukaszgogul@gmail.com>
2022-11-03 00:08:51 +0100

    add translation days dict en to pl

--- translation.py
+++ translation.py
@@ -1,8 +1,24 @@
-dict_en = {
-
+dict_en_to_pl_days = {
+    "Mo": "Pon",
+    "Mon": "Pon",
+    "Tu": "Wt",
+    "Tue": "Wt",
+    "Tues": "Wt",
+    "We": "Śr",
+    "Wed": "Śr",
+    "Th": "Czw",
+    "Thu": "Czw",
+    "Thur": "Czw",
+    "Thurs": "Czw",
+    "Fr": "Pt",
+    "Fri": "Pt",
+    "Sa": "Sob",
+    "Sat": "Sob",
+    "Su": "Niedz",
+    "Sun": "Niedz",
 }
 
-dict_pl = {
+dict_en_to_pl_months = {
 
 }
 
